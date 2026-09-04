857a81371e5cfdb2387543c0ae308fcf187c79bf
Jatinder Sahota <jsahota751@gmail.com>
2026-09-04 11:20:57 -0500

    update lsp conf

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -13,7 +13,60 @@ vim.lsp.config("clangd", {
     },
 })
 
+vim.lsp.config("html", {
+    cmd = { "vscode-html-language-server", "--stdio" },
+    filetypes = { "html" },
+    root_markers = {
+        "package.json",
+        ".git",
+    },
+})
+
+vim.lsp.config("cssls", {
+    cmd = { "vscode-css-language-server", "--stdio" },
+    filetypes = { "css" },
+    root_markers = {
+        "package.json",
+        ".git",
+    },
+})
+
+vim.lsp.config("ts_ls", {
+    cmd = { "typescript-language-server", "--stdio" },
+    filetypes = {
+        "typescript",
+        "typescriptreact",
+    },
+    root_markers = {
+        "tsconfig.json",
+        "package.json",
+        ".git",
+    },
+})
+
+vim.lsp.config("jsonls", {
+    cmd = { "vscode-json-language-server", "--stdio" },
+    filetypes = { "json", "jsonc" },
+    root_markers = { "package.json", ".git" },
+})
+
+vim.lsp.config("astro", {
+    cmd = { "astro-ls", "--stdio" },
+    filetypes = { "astro" },
+    root_markers = {
+        "astro.config.mjs",
+        "astro.config.ts",
+        "package.json",
+        ".git",
+    },
+})
+
 vim.lsp.enable({
     "lua_ls",
     "clangd",
+    "ts_ls",
+    "astro",
+    "html",
+    "cssls",
+    "jsonls"
 })
